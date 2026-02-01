d693360c39fc06d3bff027e5c81ca5e94d01405a
omedcodes <omedsafi268@gmail.com>
2026-02-01 11:11:14 +0100

    fix: unnessecary styles for bookmark-remove button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /**
- * Main Application Entry Point
- * Initializes all modules and event listeners
+ * @file index.js
+ * @fileoverview Store bookmarks from input fields to localstorage and provide <a> tag links to them.
  */
 
+// debug
 document.addEventListener("DOMContentLoaded", () => {
   console.info("Application initialized");
 });
+
