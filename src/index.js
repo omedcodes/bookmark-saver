a370fd51bfcd680a021f4b0e3e6976fa2dc4cb54
omedcodes <omedsafi268@gmail.com>
2026-02-01 10:40:29 +0100

    chore: project setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Main Application Entry Point
+ * Initializes all modules and event listeners
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.info("Application initialized");
+});
